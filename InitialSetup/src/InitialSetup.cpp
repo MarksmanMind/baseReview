1fffea8d4e6dd240b9639359b23981dbbbc73815
Gustavo Romanha Belumat <GustavoRomanha@Live.com>
2018-12-03 13:48:33 -0200

    Revert "Using temporary variables example [Number Swap]."
    This reverts commit 2155f4b730f0c74b2781fbf7854f02396be31130.

--- InitialSetup/src/InitialSetup.cpp
+++ InitialSetup/src/InitialSetup.cpp
@@ -12,19 +12,5 @@ using namespace std;
 int main() {
 	cout << "!!!Hello World!!!" << endl;
 	cout << "We must stand it until humans put their foot on mars!" << endl;
-
-	cout << "Let's make a joke by swapping numbers." << endl;
-    int first = 5, second = 10, temp;
-
-    cout << "Before swapping." << endl;
-    cout << "first = " << first << ", second = " << second << endl;
-
-    temp = first;
-    first = second;
-    second = temp;
-
-    cout << "\nAfter swapping." << endl;
-    cout << "Now the first became = " << first << ", and the second becomes = " << second << endl;
-
 	return 0;
 }
