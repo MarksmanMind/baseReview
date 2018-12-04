5493357d295cd94bebf12374f7b11dded9887d29
Vagner Cazarotto <vagnercazarotto@gmail.com>
2018-12-04 21:03:56 +0100

    Removed the initial example and add a number truncate example + how to enable cpp 11++

--- InitialSetup/src/InitialSetup.cpp
+++ InitialSetup/src/InitialSetup.cpp
@@ -10,34 +10,15 @@
 using namespace std;
 
 
-double square(double x){
-	return x*x;
-}
-
-void print_square(double x){
-	cout << "the square of " << x << " is " << square(x) << endl;
-}
-
-
 int main() {
-	cout << "!!!Hello World!!!" << endl;
-	cout << "We must stand it until humans put their foot on mars!" << endl;
 
-	cout << "Let's make a joke by swapping numbers." << endl;
-    int first = 5, second = 10, temp;
-
-    cout << "Before swapping." << endl;
-    cout << "first = " << first << ", second = " << second << endl;
-
-    temp = first;
-    first = second;
-    second = temp;
-
-    cout << "After swapping." << endl;
-    cout << "Now the first became = " << first << ", and the second becomes = " << second << endl;
-
-    cout << "call the print square function " << endl;
-    print_square(1.234);
+	double d1{2.3};
+	int i = 2;
+	d1 = d1 + i;
+	// here we truncat back to integer
+	i = d1*i;
+	cout << "normal double number ..  " << d1<< endl;
+	cout << "Just to demostrate a truncated number ..  " << i << endl;
 
 	return 0;
 }
